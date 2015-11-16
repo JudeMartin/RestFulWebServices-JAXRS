61428a76de5b44e0a4e8efa5590bbf7401fd9b28
JudeMartin <judemartin.mj@gmail.com>
2015-11-15 18:21:51 -0800

    added Delegation

--- src/main/java/org/koushik/javabrains/messenger/resources/CommentResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/CommentResource.java
@@ -2,6 +2,12 @@ package org.koushik.javabrains.messenger.resources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+/*
+ * Child Resource 
+ * 
+ * */
+
 
 @Path("/comments")
 public class CommentResource {
@@ -9,5 +15,15 @@ public class CommentResource {
 	public String test(){
 		return "new Sub Resource";
 	} 
+	
+	/**
+	 * the message ID is available via delegation
+	 **/
+	
+	@GET
+	@Path("/{commentId}")
+	public String test2(@PathParam("messageId") long messageID,@PathParam("commentId") long commentID){
+		return "Comment with the specific Id " + commentID + " for message "+messageID;
+	}
 	
 }
